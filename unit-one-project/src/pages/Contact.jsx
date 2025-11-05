42dea1bd23ea6087e696f595d95c5ee2f76e25a3
gfianu <delyouandme@gmail.com>
2025-11-05 18:04:54 -0500

    Add Header and Footer components as well as About, Contact, and Home pages

---
+++ unit-one-project/src/pages/Contact.jsx
@@ -0,0 +1,10 @@
+function Contact() {
+  return (
+    <section>
+      <h2>Contact Us</h2>
+      <p>React out for feedback, collaboration, or questions.</p>
+    </section>
+  );
+}
+
+export default Contact;
---
+++ unit-one-project/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <section>
+      <h2>Welcome to ChemLearn</h2>
+      <p>Your resource for mastering Organic Chemistry and more.</p>
+    </section>
+  );
+}
+
+export default Home;
