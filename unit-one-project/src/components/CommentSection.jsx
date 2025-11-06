4bbbac2ba8c948140ad9478c73c2b4468f431466
gfianu <delyouandme@gmail.com>
2025-11-05 20:24:09 -0500

    Button and CommentSection components created.

---
+++ unit-one-project/src/components/CommentSection.jsx
@@ -0,0 +1,10 @@
+function CommentSection() {
+  return (
+    <section>
+      <h3>Comments</h3>
+      <p>Placeholder for discussion thread below lectures.</p>
+    </section>
+  );
+}
+
+export default CommentSection;
