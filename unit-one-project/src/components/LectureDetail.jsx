c3b89cedb0d3cfac6a189221f20378673066a8f9
gfianu <delyouandme@gmail.com>
2025-11-05 19:56:38 -0500

    LectureDetail and QuizList components created.

---
+++ unit-one-project/src/components/LectureDetail.jsx
@@ -0,0 +1,11 @@
+
+function LectureDetail(){
+    return (
+        <section>
+            <h2>Lecture Title</h2>
+            <p>Placeholder for embedded video and notes.</p>
+        </section>
+    )
+}
+
+export default LectureDetail;
