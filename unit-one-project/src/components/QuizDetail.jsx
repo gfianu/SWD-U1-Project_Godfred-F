c6ea3e298bf87401d69269e4bfe5282cfca5c4a7
gfianu <delyouandme@gmail.com>
2025-11-05 20:13:54 -0500

    QuizDetail and Dashboard components created

---
+++ unit-one-project/src/components/QuizDetail.jsx
@@ -0,0 +1,10 @@
+function QuizDetail() {
+  return (
+    <section>
+      <h2>Quiz Title</h2>
+      <p>Placeholder for quiz questions and answers.</p>
+    </section>
+  );
+}
+
+export default QuizDetail;
