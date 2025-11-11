09010c800fe92e54f7974fb31af00d2a028bd041
gfianu <delyouandme@gmail.com>
2025-11-11 06:46:56 -0500

    routing and layout updated

--- unit-one-project/src/components/NavBar.jsx
+++ unit-one-project/src/components/NavBar.jsx
@@ -1,12 +1,27 @@
+import { NavLink } from "react-router";
+
 function NavBar() {
   return (
     <nav>
       <ul>
-        <li>Home</li>
-        <li>Lectures</li>
-        <li>Quizzes</li>
-        <li>Dashboard</li>
-        <li>About</li>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/lectures">Lectures</NavLink>
+        </li>
+        <li>
+          <NavLink to="/quizzes">Quizzes</NavLink>
+        </li>
+        <li>
+          <NavLink to="/dashboard">Dashboard</NavLink>
+        </li>
+        <li>
+          <NavLink to="/about">About</NavLink>
+        </li>
+        <li>
+          <NavLink to="/contact">Contact</NavLink>
+        </li>
       </ul>
     </nav>
   );
